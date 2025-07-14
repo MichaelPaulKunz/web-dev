1e608ad770d95bfa7b7f326156569aa0e3e06f4f
scriptkd <michaelpaulkunz@gmail.com>
2025-07-14 14:51:34 -0400

    fetch function

--- public/webchat/index.js
+++ public/webchat/index.js
@@ -703,10 +703,7 @@ function officeClosed() {
 }
 
 window.addEventListener('beforeunload', (event) => {
-   // Most modern browsers require preventDefault() to show the confirmation dialog
-   event.preventDefault(); 
-   // For older browsers, you might also need to set returnValue
-   event.returnValue = 'Are you sure you want to leave? Your changes may not be saved.';
+   fetch('https://tcom-twilio-service-9295-dev.twil.io/chat/conversations-end-chat');
  });
 /**
  *  const line_1 = document.getElementsByClassName('css-11qrk9w')[0];
